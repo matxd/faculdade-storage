164ca19cade7d827a95e7698efec12a1260cabfe
Matheus <matheusrcrv07@gmail.com>
2022-08-10 18:25:16 -0300

    Conceitos básicos resolvidos

---
+++ Lógica de Programação e Algoritmos/Lista Exercicios - Unidade I e II/main.py
@@ -0,0 +1,18 @@
+# I. Conceitos Básicos
+# - A)
+mostrarDobro = 7 * 2
+print(mostrarDobro)
+
+# B)
+numeroUm = 5
+numeroDois = 7
+print(numeroUm + numeroDois, numeroUm - numeroDois, numeroUm * numeroDois, numeroUm / numeroDois)
+
+# C)
+idadeEmAnos = 30
+print(idadeEmAnos * 365, "dias")
+
+# D)
+salarioBruto = 1500
+totalVendas = 1000
+print(salarioBruto + ((totalVendas * 15) / 100))
